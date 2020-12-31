7ebf5dee1f09519e2ab2cca2ed1a81212ae3ed6d
TurtleWolfe <JonPohlner@gmail.com>
2020-12-30 19:15:58 -0500

    3-10 count down functional

--- src/components/Toggel/Toggle.js
+++ src/components/Toggel/Toggle.js
@@ -9,49 +9,58 @@ export class Toggle extends Component {
     this.state = {
       tglStart: "Start",
       tglVariant: 'btn-success',
+      timeLft: 3,
     };
-
     // This binding is necessary to make `this` work in the callback
-    this.setToggleOnOff = this.setToggleOnOff.bind(this);
-
+    // this.setToggleOnOff = this.setToggleOnOff.bind(this);
+    // or not necessary if using an arrow function..
   }
 
   static propTypes = {
     tglStart: PropTypes.string.isRequired,
     tglVariant: PropTypes.string.isRequired,
+    timeLft: PropTypes.number.isRequired,
   };
 
-  // setToggleOnOff() {
-  //   let { tglStart } = this.state;
-  //   if (tglStart === "Start") {
-  //     this.setState({
-  //       tglStart: "Stop",
-  //       tglVariant: `"danger"`,
-  //     });
-  //   } else {
-  //     this.setState({
-  //       tglStart: "Start",
-  //       tglVariant: `"success"`,
-  //     });
-  //   }
-  // }
-
-  setToggleOnOff() {
+  setToggleOnOff = () => {
     let { tglStart } = this.state;
-    (tglStart === "Start") ?
+    if (tglStart === "Start") {
+      this.interval = setInterval(this.countDown, 1000);
       this.setState({
         tglStart: "Stop",
         tglVariant: 'btn-danger',
-      }) :
+      });
+    } else {
       this.setState({
         tglStart: "Start",
         tglVariant: 'btn-success',
       });
-  }
+      // Clearing the interval
+      clearInterval(this.interval);
+    }
+  };
+
+  countDown = () => {
+    // If the time reach 0 then we display Buzzzz! alert.
+    if (this.state.timeLft === 0) {
+      this.setState({
+        tglStart: "Start",
+        tglVariant: 'btn-success',
+        timeLft: 10
+      });
+      clearInterval(this.interval);
+    } else {
+      // We decrease the time second by second
+      this.setState({
+        timeLft: this.state.timeLft - 1
+      });
+    }
+  };
 
   render() {
     let { tglStart } = this.state;
     let { tglVariant } = this.state;
+    let { timeLft } = this.state;
     return (
       <Container>
         <Row className="justify-content-center">
@@ -61,6 +70,15 @@ export class Toggle extends Component {
               {tglStart}
               <br></br>
               {tglVariant}
+            </h4>
+          </Col>
+        </Row>
+        <br></br>
+        <Row className="justify-content-center">
+          <Col as={Button} className={tglVariant} xs={4} sm={4} md={3} lg={2}
+            onClick={this.setToggleOnOff}>
+            <h4>
+              {timeLft}
             </h4>
           </Col>
         </Row>
